bf48ef61e8085a3232ed7dc155f217419383115e
Andrea <a21andreapp@iessanclemente.net>
2023-03-05 16:14:37 +0100

    arreglo problema cliente

--- axencia_viaxes/models/client.py
+++ axencia_viaxes/models/client.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import date
 
 class Client(models.Model):
     _name = 'agency.client'
     _inherits = {'res.partner': 'partner_id'}
     _description = "Cliente da axencia"
 
-    partner_id = fields.Many2one('res.partner', ondelete='cascade')
+    partner_id = fields.Many2one('res.partner', ondelete='cascade', string='Nome')
     member_number = fields.Integer(required=True)
     date_of_birth = fields.Date('Data de nacemento')
     years = fields.Integer('Anos', compute='calculate_age')
 
     @api.depends('date_of_birth')
     def calculate_age(self):
-        today = date.today()
-        age = today.year - self.date_of_birth.year - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
-        return age
+        for record in self:
+            try:
+                today = date.today()
+                birth_date = record.date_of_birth
+                age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+                record.years = age
+            except:
+                record.years = 0
