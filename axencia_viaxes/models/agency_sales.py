d1105a67a239c00b38480f22221b939967a8a281
Andrea <a21andreapp@iessanclemente.net>
2023-03-12 15:51:02 +0100

    cambiado nome que aparece en ventas

--- axencia_viaxes/models/agency_sales.py
+++ axencia_viaxes/models/agency_sales.py
@@ -18,7 +18,13 @@ class AgencySales(models.Model):
     activities = fields.Many2many('agency.activity', string='Lista de actividades', required=True)
     data_compra = fields.Date('Data compra', readonly=True, compute='set_date')
     pagado = fields.Boolean('Pagado', default=False)
+    name = fields.Char(compute='_compute_name', store=True)
 
     def set_date(self):
         for sales in self:
             sales.data_compra = datetime.now()
+
+    @api.depends('client_name', 'flight_name')
+    def _compute_name(self):
+        for record in self:
+            record.name = f"{self.client_name.partner_id.name} ( {self.flight_name.departure_point.location} - {self.flight_name.destination_point.location})"
